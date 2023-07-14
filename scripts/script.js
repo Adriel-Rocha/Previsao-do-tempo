b67fddc0421ce5b226d8c0f41c9a1dfddc8b49af
Adriel Francisco Rocha <67209277+Adriel-Rocha@users.noreply.github.com>
2023-07-14 11:19:52 -0300

    weather version in portuguese

---
+++ scripts/script.js
@@ -0,0 +1,71 @@
+
+
+const key = "4d27bcad02daf8dab91104c47dab06db"
+const listCity = ['New York', 'California', 'Brasília', 'São Paulo', 'New Jersey', 'Tokio', 'Toronto', 'Vancouver', 'Montreal', 'Moscou', 'São Petersburgo', 'Cancún', 'Buenos Aires', 'Lisboa', 'Porto', 'Coimbra', 'Aveiro']
+const cityRandom = listCity[Math.floor(Math.random() * listCity.length)]
+const googleApiKey = "AIzaSyDUdznwW-wynsFrxiF7mKlvCFHaWYwW9E0"
+const searchEngineId = "f1ad8c9fbb70f4923"
+
+
+
+function clickSearch() {
+    const city = document.querySelector(".inputCity").value
+    searchCity(city)
+    searchImageCity(city)
+}
+
+
+async function searchCity(city) {
+    const dates = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then(response => response.json())
+    showResult(dates)
+}
+
+
+function showResult(dates) {
+    document.querySelector('.city').innerHTML = 'Clima em ' + dates.name
+    document.querySelector('.temp').innerHTML = Math.floor(dates.main.temp) + '°C'
+    document.querySelector('.imgPreview').src = `https://openweathermap.org/img/wn/${dates.weather[0].icon}.png`
+    document.querySelector('.textPreview').innerHTML = dates.weather[0].description
+    document.querySelector('.humidity').innerHTML = 'Umidade: ' + dates.main.humidity + '%'
+    document.querySelector('.inputCity').value = ''
+}
+
+
+window.onload =  function openPage() {
+    searchCity(cityRandom)
+    searchImageCity(cityRandom)
+}
+
+
+
+
+
+
+
+
+
+async function searchImageCity(city) {
+    const dates = await fetch(`https://www.googleapis.com/customsearch/v1?key=${googleApiKey}&cx=${searchEngineId}&searchType=image&imgSize=large&num=1&q=imagem da cidade de ${city}`).then(response => response.json())
+    const imgBackground = dates.items[0].link
+    changTheBackgraund(imgBackground)
+
+}
+    
+function changTheBackgraund(imgBackground) {
+    document.body.style.backgroundImage = `url(${imgBackground})`
+}
+
+
+
+
+
+// async function getImage(cityName) {
+//     const datesOnSearch = await fetch(`https://www.googleapis.com/customsearch/v1?key=${googleApiKey}&cx=${searchEngineId}&searchType=image&imgSize=xlarge&num=1&q=imagem da cidade de ${cityName}`).then(response => response.json())
+//     const linkImg = datesOnSearch.items[0].link
+//     backGroundImg(linkImg)
+// }
+
+// function backGroundImg(imgLink) {
+//     document.body.style.backgroundImage = `url(${imgLink})`
+//     console.log(document.body.style.backgroundImage)
+// }
