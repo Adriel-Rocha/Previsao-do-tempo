e903ef1b6def0b9da173f95e48c7ed83012a9063
Adriel Francisco Rocha <67209277+Adriel-Rocha@users.noreply.github.com>
2025-06-17 10:39:31 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,58 +1,44 @@
+window.onload =  function openPage() {
 
+    const listCity = ['New York', 'California', 'Brasília', 'São Paulo', 'New Jersey', 'Tokio', 'Toronto', 'Vancouver', 'Montreal', 'Moscou', 'São Petersburgo', 'Cancún', 'Buenos Aires', 'Lisboa', 'Porto', 'Coimbra', 'Aveiro'];
+    const cityRandom = listCity[Math.floor(Math.random() * listCity.length)];
 
-const key = "4d27bcad02daf8dab91104c47dab06db"
-const listCity = ['New York', 'California', 'Brasília', 'São Paulo', 'New Jersey', 'Tokio', 'Toronto', 'Vancouver', 'Montreal', 'Moscou', 'São Petersburgo', 'Cancún', 'Buenos Aires', 'Lisboa', 'Porto', 'Coimbra', 'Aveiro']
-const cityRandom = listCity[Math.floor(Math.random() * listCity.length)]
-const googleApiKey = "AIzaSyDUdznwW-wynsFrxiF7mKlvCFHaWYwW9E0"
-const searchEngineId = "f1ad8c9fbb70f4923"
-
-
+    searchCity(cityRandom);
+}
 
 function clickSearch() {
-    const city = document.querySelector(".inputCity").value
-    searchCity(city)
-    searchImageCity(city)
-}
 
+    const city = document.querySelector(".inputCity").value;
+    searchCity(city);
+}
 
 async function searchCity(city) {
-    const dates = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then(response => response.json())
-    showResult(dates)
-}
 
+    const res = await fetch(`https://weather-backend-utcz.onrender.com/forecast?city=${city}&?lang=pt-br`);
+    const dates = await res.json();
+    showResult(dates);
+}
 
 function showResult(dates) {
-    document.querySelector('.city').innerHTML = 'Clima em ' + dates.name
-    document.querySelector('.iconFlag').src = `https://flagsapi.com/${dates.sys.country}/shiny/64.png`
-    document.querySelector('.temp').innerHTML = Math.floor(dates.main.temp) + '°C'
-    document.querySelector('.imgPreview').src = `https://openweathermap.org/img/wn/${dates.weather[0].icon}.png`
-    document.querySelector('.textPreview').innerHTML = dates.weather[0].description
-    document.querySelector('.humidity').innerHTML = dates.main.humidity + '%'
-    document.querySelector('.air').innerHTML = dates.wind.speed + 'm/s'
-    document.querySelector('.inputCity').value = ''
-}
 
+    const city = dates.weather.name;
+    const iconFlag = `https://flagsapi.com/${dates.weather.sys.country}/shiny/64.png`;
+    const temp = dates.weather.main.temp;
+    const imgPreview = `https://openweathermap.org/img/wn/${dates.weather.weather[0].icon}.png`;
+    const description = dates.weather.weather[0].description;
+    const humidity = dates.weather.main.humidity;
+    const air = dates.weather.wind.speed;
 
-window.onload =  function openPage() {
-    searchCity(cityRandom)
-    searchImageCity(cityRandom)
-}
+    const bgImage = `url(${dates.image})`;
 
+    document.querySelector('.city').innerHTML = `Clima em ${city}`;
+    document.querySelector('.iconFlag').src = iconFlag;
+    document.querySelector('.temp').innerHTML = `${temp}°C`;
+    document.querySelector('.imgPreview').src = imgPreview;
+    document.querySelector('.textPreview').innerHTML = description;
+    document.querySelector('.humidity').innerHTML = `${humidity}%`;
+    document.querySelector('.air').innerHTML = `${air} m/s`;
+    document.querySelector('.inputCity').value = '';
 
-
-
-
-
-
-
-
-async function searchImageCity(city) {
-    const dates = await fetch(`https://www.googleapis.com/customsearch/v1?key=${googleApiKey}&cx=${searchEngineId}&searchType=image&imgSize=large&num=1&q=imagem da cidade de ${city}`).then(response => response.json())
-    const imgBackground = dates.items[0].link
-    changTheBackgraund(imgBackground)
-
-}
-    
-function changTheBackgraund(imgBackground) {
-    document.body.style.backgroundImage = `url(${imgBackground})`
+    document.body.style.backgroundImage = bgImage;
 }
